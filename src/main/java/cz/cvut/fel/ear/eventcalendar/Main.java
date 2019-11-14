07199316519c7317d56d3e98e2ba416ef925be07
stepaego <stepaego@fel.cvut.cz>
2019-11-14 18:03:25 +0100

    test2

--- src/main/java/cz/cvut/fel/ear/eventcalendar/Main.java
+++ src/main/java/cz/cvut/fel/ear/eventcalendar/Main.java
@@ -2,6 +2,6 @@ package cz.cvut.fel.ear.eventcalendar;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("asdasdasd");
+        System.out.println("testtesttest");
     }
 }
